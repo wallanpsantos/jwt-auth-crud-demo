90f1d5433a4335702487b091f8714fa0d3c6967b
wallapsantos <wallanpereira09@gmail.com>
2023-11-21 00:08:37 -0300

    feat: Projeto Base

---
+++ src/main/java/com/example/jwtauthcruddemo/JwtAuthCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthCrudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwtauthcruddemo/JwtAuthCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
