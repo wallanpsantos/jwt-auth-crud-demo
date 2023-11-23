1ae3deebc1935638c47cb4a0ac58519f501ab175
wallapsantos <wallanpereira09@gmail.com>
2023-11-23 14:16:10 -0300

    Base

--- src/test/java/com/jwtauthcruddemo/JwtAuthCrudDemoApplicationTests.java
+++ src/test/java/com/jwtauthcruddemo/JwtAuthCrudDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.jwtauthcruddemo;
+package com.jwtauthcruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JwtAuthCrudDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
