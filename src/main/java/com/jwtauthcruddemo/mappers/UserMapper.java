91f02c3180f84d2fbd1baffda4ec4178d861e435
wallapsantos <wallanpereira09@gmail.com>
2023-11-28 13:05:22 -0300

    feat: Add PasswordConfig and update UserMapper

--- src/main/java/com/jwtauthcruddemo/mappers/UserMapper.java
+++ src/main/java/com/jwtauthcruddemo/mappers/UserMapper.java
@@ -3,11 +3,8 @@ package com.jwtauthcruddemo.mappers;
 import com.jwtauthcruddemo.dtos.output.UserDto;
 import com.jwtauthcruddemo.entities.UserEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    @Mapping(target = "token", ignore = true)
     UserDto toUserDto(UserEntity userEntity);
 }
