ca02a5415d86f5b69d14b33fdc3e8fdfd847d0c3
wallapsantos <wallanpereira09@gmail.com>
2023-11-28 12:06:35 -0300

    feat: Respository; Postgresql; docker-compose;

---
+++ src/main/java/com/jwtauthcruddemo/entities/UserEntity.java
@@ -0,0 +1,39 @@
+package com.jwtauthcruddemo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "user_table")
+public class UserEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email(message = "Email deve ser válido")
+    private String email;
+
+    @NotBlank
+    private String password;
+}
