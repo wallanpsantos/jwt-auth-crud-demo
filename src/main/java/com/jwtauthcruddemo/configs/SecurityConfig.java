b4abf95181066e3d14f942c84fa61b31c01edc53
wallapsantos <wallanpereira09@gmail.com>
2023-12-07 15:22:16 -0300

    feat: Configurando JWT Double Check

--- src/main/java/com/jwtauthcruddemo/configs/SecurityConfig.java
+++ src/main/java/com/jwtauthcruddemo/configs/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,29 +18,35 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     private final LoginAuthProvider loginAuthProvider;
+    private final LoginAuthenticationEntryPoint loginAuthenticationEntryPoint;
 
-    public SecurityConfig(LoginAuthProvider loginAuthProvider) {
+    public SecurityConfig(LoginAuthProvider loginAuthProvider, LoginAuthenticationEntryPoint loginAuthenticationEntryPoint) {
         this.loginAuthProvider = loginAuthProvider;
+        this.loginAuthenticationEntryPoint = loginAuthenticationEntryPoint;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                // Filtro antes do filtro de autenticação básico, porque o JWTAuthFilter desejo que seja primeiro filtro.
-                .addFilterBefore(new JWTAuthFilter(loginAuthProvider), BasicAuthenticationFilter.class)
-                .sessionManagement(SecurityConfig::configSessionManagement)
-                .authorizeHttpRequests(SecurityConfig::configAuthorizeHttpRequests);
-
+        httpSecurity.exceptionHandling(this::configLoginAuthenticationEntryPoint);
+        httpSecurity.addFilterBefore(new JWTAuthFilter(loginAuthProvider), BasicAuthenticationFilter.class); // Filtro antes do filtro de autenticação básico, porque o JWTAuthFilter desejo que seja primeiro filtro.
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.sessionManagement(SecurityConfig::configSessionManagement);
+        httpSecurity.authorizeHttpRequests(SecurityConfig::configAuthorizeHttpRequests);
         return httpSecurity.build();
     }
 
     private static void configAuthorizeHttpRequests(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
-        authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.POST, "/v1/login", "/v1/register").permitAll()
-                .anyRequest().authenticated();
+        authorizationManagerRequestMatcherRegistry.requestMatchers(HttpMethod.POST, "/v1/login", "/v1/register")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 
     private static void configSessionManagement(SessionManagementConfigurer<HttpSecurity> management) {
         management.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    private void configLoginAuthenticationEntryPoint(ExceptionHandlingConfigurer<HttpSecurity> customizer) {
+        customizer.authenticationEntryPoint(loginAuthenticationEntryPoint);
+    }
 }
