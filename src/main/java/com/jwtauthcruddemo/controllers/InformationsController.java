ba698e5d97dc34ed3e55f23d45f36e915807296d
wallapsantos <wallanpereira09@gmail.com>
2023-12-07 15:35:40 -0300

    feat: mudança de endpoint

--- src/main/java/com/jwtauthcruddemo/controllers/InformationsController.java
+++ src/main/java/com/jwtauthcruddemo/controllers/InformationsController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/messages")
+@RequestMapping("/v1/informations")
 public class InformationsController {
 
     @GetMapping
     public ResponseEntity<List<String>> informations() {
-        return ResponseEntity.ok(List.of("Porjeto", "Backend com JWT"));
+        return ResponseEntity.ok(List.of("Projeto", "Backend com JWT", "FullStack"));
     }
 
 }
